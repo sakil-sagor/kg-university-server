7fa22c384270eefedc58df2b16225dc2b4efe279
sakil-sagor <sakilsagor76543@gmail.com>
2025-05-05 14:19:36 +0600

    update user controller and services

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,24 @@
+import config from '../../config';
 import { TStudent } from '../student/student.interface';
+import { Student } from '../student/student.model';
+import { TUser } from './user.interface';
 import { User } from './user.model';
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
-  const result = await User.create(studentData);
-  return result;
+  const userData: Partial<TUser> = {};
+  userData.password = password || (config.default_passwrod as string);
+  userData.role = 'student';
+  //   set user id manually
+
+  const newUser = await User.create(userData);
+
+  if (Object.keys(newUser).length) {
+    studentData.id = newUser.id;
+    studentData.user = newUser._id; //reference _id
+    const newStudent = await Student.create(studentData);
+    return newStudent;
+  }
+  return newUser;
 };
 
 export const UserServices = {
