81b92cd68ec2653ac5b92ace101a93aae4415afb
sakil-sagor <sakilsagor76543@gmail.com>
2025-05-07 15:56:40 +0600

    update some issue

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,9 +4,9 @@ import { UserControllers } from './user.controller';
 const router = express.Router();
 
 router.post('/create-student', UserControllers.createStudent);
-router.get('/', UserControllers.getallUser);
-router.get('/:userId', UserControllers.getSingleUser);
-router.delete('/:userId', UserControllers.deleteUser);
-router.patch('/:userId', UserControllers.updateUser);
+// router.get('/', UserControllers.getallUser);
+// router.get('/:userId', UserControllers.getSingleUser);
+// router.delete('/:userId', UserControllers.deleteUser);
+// router.patch('/:userId', UserControllers.updateUser);
 
 export const UserRoutes = router;
